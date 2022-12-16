cd2f2e82d921c5a8e4b1ad0282e8122ee2f8b484
Chris Bryant <chrisbryant.dev@gmail.com>
2022-12-16 05:29:23 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./styles.css";
+
+const person = {
+  firstName: ["Chris", "John", "Madison", "Todd", "Dale"],
+  lastName: ["Bryant", "Bond", "Bryant", "Williamson", "Sparks"],
+  score: [88, 98, 99, 76, 67]
+};
+
+let combinedArray = [];
+for (let i = 0; i < person.firstName.length; i++) {
+  combinedArray.push(i);
+}
+
+const personArray = (() => {
+  const { firstName, lastName, score } = person;
+
+  return combinedArray.map((i) => {
+    const format =
+      i + 1 + ": " + firstName[i] + " " + lastName[i] + " " + "[Score: ";
+
+    return (
+      <li key={i}>
+        <span>{format}</span>
+        <strong>{score[i]}</strong>
+        <span>]</span>
+      </li>
+    );
+  });
+})();
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>
+        <u>People List:</u>
+      </h2>
+      <ul className="list">{personArray}</ul>
+    </div>
+  );
+}
